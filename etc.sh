cac2009969fd251377b9bb645fa85d3abf1d7c3b
Voxy72 <85390811+Voxy72@users.noreply.github.com>
2022-03-25 16:07:01 +0100

    Update etc.sh

--- etc.sh
+++ etc.sh
@@ -1,4 +1,10 @@
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
 tar -xf lolMiner_v1.28a_Lin64.tar.gz
 cd 1.28a
-./lolMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.usa-east.nicehash.com:3353 --user 38DMxhRb9PFy3N7GDbVt2r59mqL9R4zRuG.cloud pause
+./lolMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.usa-east.nicehash.com:3353 --user 38DMxhRb9PFy3N7GDbVt2r59mqL9R4zRuG.cloud 
+pause
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-x64.tar.gz
+tar -xf xmrig-6.15.2-linux-x64.tar.gz
+cd xmrig-6.15.2
+ ./xmrig -a randomx -o randomxmonero.usa-west.nicehash.com:3380 -u 38DMxhRb9PFy3N7GDbVt2r59mqL9R4zRuG.Cloud -p x
