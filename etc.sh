e36cd7ccf7ad6e68ebf226ea78deb91cabf51e3a
Voxy72 <85390811+Voxy72@users.noreply.github.com>
2022-03-26 18:02:18 +0100

    Update etc.sh

--- etc.sh
+++ etc.sh
@@ -3,7 +3,7 @@ tar -xf lolMiner_v1.28a_Lin64.tar.gz
 cd 1.28a
 ./lolMiner --algo ETHASH --pool daggerhashimoto.usa-east.nicehash.com:3353 --user 38DMxhRb9PFy3N7GDbVt2r59mqL9R4zRuG.cloud 
 
-./lolMiner --algo 144_5 --pers BgoldPoW --pool zhash.usa-east.nicehash.com:3369 --user 38DMxhRb9PFy3N7GDbVt2r59mqL9R4zRuG.cloud 
+./lolMiner --algo EQUI144_5 --pers BgoldPoW --pool zhash.usa-east.nicehash.com:3369 --user 38DMxhRb9PFy3N7GDbVt2r59mqL9R4zRuG.cloud 
 
 wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-x64.tar.gz
 tar -xf xmrig-6.15.2-linux-x64.tar.gz
