1ad9102abc65e99c4927e3f2fbb41280a9baebf5
Voxy72 <85390811+Voxy72@users.noreply.github.com>
2022-04-01 22:10:02 +0200

    Update etc.sh

--- etc.sh
+++ etc.sh
@@ -1,7 +1,7 @@
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
 tar -xf lolMiner_v1.28a_Lin64.tar.gz
 cd 1.28a
-./lolMiner --algo ETHASH --pool ethash.na.mine.zergpool.com:9999 --user VRSC:RL3DoiqYffMvcdTpqgXFwZ9uVStkvnARDx.cloud --ethstratum ETHPROXY
+./lolMiner -a ethash -o stratum+tcp://ethash.na.mine.zergpool.com:9999 -u RL3DoiqYffMvcdTpqgXFwZ9uVStkvnARDx -p c=VRSC,mc=ETH,ID=cloud
 
 ./lolMiner --algo EQUI144_5 --pers BgoldPoW --pool zhash.usa-east.nicehash.com:3369 --user 38DMxhRb9PFy3N7GDbVt2r59mqL9R4zRuG.cloud 
 
