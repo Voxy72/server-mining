6973c580800687a6290b3ec9704fdf1674d9ea05
Voxy72 <85390811+Voxy72@users.noreply.github.com>
2022-05-01 21:54:10 +0200

    Update etc.sh

--- etc.sh
+++ etc.sh
@@ -5,7 +5,3 @@ cd 1.28a
 
 ./lolMiner --algo EQUI144_5 --pers BgoldPoW --pool zhash.usa-east.nicehash.com:3369 --user 38DMxhRb9PFy3N7GDbVt2r59mqL9R4zRuG.cloud 
 
-wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-x64.tar.gz
-tar -xf xmrig-6.15.2-linux-x64.tar.gz
-cd xmrig-6.15.2
- ./xmrig -a randomx -o randomxmonero.usa-west.nicehash.com:3380 -u 38DMxhRb9PFy3N7GDbVt2r59mqL9R4zRuG.Cloud -p x
