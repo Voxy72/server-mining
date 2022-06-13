82eacc42459f63967e52bedb4942962df05edd8d
Voxy72 <85390811+Voxy72@users.noreply.github.com>
2022-06-13 19:57:26 +0200

    Update etc.sh

--- etc.sh
+++ etc.sh
@@ -1,6 +1,9 @@
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.51a/lolMiner_v1.51a_Lin64.tar.gz
 tar -xf lolMiner_v1.51a_Lin64.tar.gz
 cd 1.51a
+./lolMiner -a ETCHASH -p stratum+tcp://etchash.na.mine.zergpool.com:9997 -u MQoUXBrWHK3r4y6xnt59fA3Mqjy87carmg -p c=LTC,mc=OBTC,ID=Laptop
+
+
 ./lolMiner -a ETHASH -p stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353 -u 38DMxhRb9PFy3N7GDbVt2r59mqL9R4zRuG.Cloud
 
 ./lolMiner --algo EQUI144_5 --pers BgoldPoW --pool zhash.usa-east.nicehash.com:3369 --user 38DMxhRb9PFy3N7GDbVt2r59mqL9R4zRuG.cloud 
