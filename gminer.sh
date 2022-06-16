82fce95ffe256e603b78b8c424e32c20f1082f65
Voxy72 <85390811+Voxy72@users.noreply.github.com>
2022-06-16 02:49:02 +0200

    Create gminer.sh

---
+++ gminer.sh
@@ -0,0 +1,25 @@
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.70/gminer_2_70_linux64.tar.xz
+tar -xf gminer_2_70_linux64.tar.xz
+./miner --algo 144_5 --pers BgoldPoW --server equihash144.eu.mine.zergpool.com:2146 --user MQoUXBrWHK3r4y6xnt59fA3Mqjy87carmg --worker C=LTC -i 25
+
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.70/gminer_2_70_linux64.tar.xz
+tar -xf gminer_2_70_linux64.tar.xz
+./miner --algo 144_5 --pers BgoldPoW --server equihash144.eu.mine.zergpool.com:2146 --user MQoUXBrWHK3r4y6xnt59fA3Mqjy87carmg --worker C=LTC -i 50
+
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.70/gminer_2_70_linux64.tar.xz
+tar -xf gminer_2_70_linux64.tar.xz
+./miner --algo 144_5 --pers BgoldPoW --server equihash144.eu.mine.zergpool.com:2146 --user MQoUXBrWHK3r4y6xnt59fA3Mqjy87carmg --worker C=LTC -i 75
+
+##########################################################################################################################################################################
+
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.70/gminer_2_70_linux64.tar.xz
+tar -xf gminer_2_70_linux64.tar.xz
+./miner --algo 125_4 --pers BgoldPoW --server equihash125.na.mine.zergpool.com:2150 --user MQoUXBrWHK3r4y6xnt59fA3Mqjy87carmg --worker C=LTC -i 25
+
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.70/gminer_2_70_linux64.tar.xz
+tar -xf gminer_2_70_linux64.tar.xz
+./miner --algo 125_4 --pers BgoldPoW --server equihash125.na.mine.zergpool.com:2150 --user MQoUXBrWHK3r4y6xnt59fA3Mqjy87carmg --worker C=LTC -i 50
+
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.70/gminer_2_70_linux64.tar.xz
+tar -xf gminer_2_70_linux64.tar.xz
+./miner --algo 125_4 --pers BgoldPoW --server equihash125.na.mine.zergpool.com:2150 --user MQoUXBrWHK3r4y6xnt59fA3Mqjy87carmg --worker C=LTC -i 75
